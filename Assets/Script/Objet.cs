da6a11967944b9c7b5ef199e19b37e6c2e72046f
Alex <alexandre.morize@hotmail.fr>
2019-10-31 15:04:18 +0100

    Correction

---
+++ Assets/Script/Objet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objet : MonoBehaviour
+{
+    public string Nom, Description;
+
+
+    protected virtual void OnMouseOver()
+    {
+        PopUp.Show(Nom, Description);
+    }
+
+    protected virtual void OnMouseExit()
+    {
+        PopUp.Hide();
+    }
+
+}
