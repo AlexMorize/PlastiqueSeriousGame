a410611e803e93fb80e5be6e5767e191f6bd1464
Alex <alexandre.morize@hotmail.fr>
2019-10-31 13:31:01 +0100

    Init

---
+++ Assets/Déchet.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Déchet : Objet
+{
+    public Collider2D collider { get; private set; }
+
+    public float polutionEngendrée;
+    public ZoneDeDechet PoubelleAffilié;
+
+    protected override void OnMouseOver()
+    {
+        if (GameManager.ActualDrag) return;
+
+        string message = "Polution: +" + polutionEngendrée.ToString() + " %" + System.Environment.NewLine +
+            "Poubelle: " + PoubelleAffilié.ToString() + System.Environment.NewLine +
+            Description;
+
+        PopUp.Show(Nom, message);
+    }
+
+    void OnMouseDown()
+    {
+        collider.enabled = false;
+        PopUp.Hide();
+        GameManager.ActualDrag = this;
+    }
+
+    private void Start()
+    {
+        collider = GetComponent<Collider2D>();
+    }
+
+}
+
+public enum ZoneDeDechet
+{
+    Générale, Tri, Déchetterie
+}
---
+++ Assets/Poubelle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Poubelle : MonoBehaviour
+{
+    public ZoneDeDechet Type;
+
+    public void OnMouseOver()
+    {
+        GameManager.CurrentPoubelle = this;
+    }
+
+    public void OnMouseExit()
+    {
+        if (GameManager.CurrentPoubelle == this) GameManager.CurrentPoubelle = null;
+    }
+
+
+}
