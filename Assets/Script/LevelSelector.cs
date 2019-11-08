255818214ad98c29e5bccab51ddf5084bd2ba6df
Alex <alexandre.morize@hotmail.fr>
2019-11-08 15:46:41 +0100

    Commencement du menu de sélection de niveau

---
+++ Assets/Script/LevelSelector.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelector : MonoBehaviour
+{
+    public SpriteRenderer terreDetruiteSprite;
+    public TextMesh textLvl;
+
+    int level = 0;
+    public void NextLevel()
+    {
+        GetComponent<Animator>().SetInteger("Direction", 1);
+        level++;
+    }
+
+    public void PreviousLevel()
+    {
+        GetComponent<Animator>().SetInteger("Direction", -1);
+        level--;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateImage()
+    {
+        GetComponent<Animator>().SetInteger("Direction", 0);
+        terreDetruiteSprite.color = new Color(1, 1, 1, level / 10f);
+        textLvl.text = level.ToString();
+    }
+}
